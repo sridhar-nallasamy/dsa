246655ad186232edbae848d302051a48eb28c137
Sridhar Nallasamy <nsridharbtech@gmail.com>
2023-10-15 17:47:11 +0530

    2600 (#1)
    * Solution added

---
+++ leetcode/easy/2600/js.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} numOnes
+ * @param {number} numZeros
+ * @param {number} numNegOnes
+ * @param {number} k
+ * @return {number}
+ */
+var kItemsWithMaximumSum = function (numOnes, numZeros, numNegOnes, k) {
+  /**
+   * Time: O(1)
+   * Space: O(1)
+   */
+  if (k <= numOnes) return k;
+
+  if (k <= numOnes + numZeros) return numOnes;
+
+  return numOnes + (k - (numOnes + numZeros)) * -1;
+};

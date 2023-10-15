6f184b566fda9d6fe2fcc876098c9d70616a958e
Sridhar Nallasamy <nsridharbtech@gmail.com>
2023-10-15 17:41:43 +0530

    complexity commented

--- leetcode/easy/2600/js.js
+++ leetcode/easy/2600/js.js
@@ -6,6 +6,10 @@
  * @return {number}
  */
 var kItemsWithMaximumSum = function (numOnes, numZeros, numNegOnes, k) {
+  /**
+   * Time: O(1)
+   * Space: O(1)
+   */
   if (k <= numOnes) return k;
 
   if (k <= numOnes + numZeros) return numOnes;

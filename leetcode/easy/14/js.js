84166a887b2a3e16adac75f7c46ee3219f213e4b
Sridhar Nallasamy <nsridharbtech@gmail.com>
2023-10-17 08:54:43 +0530

    Solution added

---
+++ leetcode/easy/14/js.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  /**
+   * Time: O(nm log n)
+   * Space: O(m)
+   *
+   * where n is the length of the array &
+   * m is the length of 1st word after sorting
+   */
+  strs.sort();
+  const leftWord = strs[0];
+  const rightWord = strs.at(-1);
+  let idx = 0;
+  let res = '';
+  while (idx < leftWord.length) {
+    if (leftWord[idx] === rightWord[idx]) {
+      res += leftWord[idx];
+      idx += 1;
+    } else {
+      break;
+    }
+  }
+  return res;
+};

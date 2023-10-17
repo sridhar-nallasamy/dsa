b3e141329aebcd74538d0244096300d14f0c63df
Sridhar Nallasamy <nsridharbtech@gmail.com>
2023-10-17 23:59:47 +0530

    Sol added

---
+++ leetcode/easy/28/js.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  /**
+   * Time: O(m+n)
+   * Space: O(1)
+   *
+   * where m = haystack length & n = needle length
+   *
+   */
+  let idx = 0;
+  while (idx < haystack.length) {
+    if (haystack[idx] === needle[0]) {
+      let i = 0;
+      while (
+        i < needle.length &&
+        idx + i < haystack.length &&
+        needle[i] === haystack[idx + i]
+      ) {
+        i += 1;
+      }
+      if (i === needle.length) return idx;
+    }
+    idx += 1;
+  }
+  return -1;
+};

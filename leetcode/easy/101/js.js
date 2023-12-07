ad59e673ccd0e3923269887cf1734413d9e75b47
Sridhar Nallasamy <nsridharbtech@gmail.com>
2023-12-07 15:26:10 +0530

    101 (#21)

---
+++ leetcode/easy/101/js.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  if (!root) return true;
+
+  const queue = [root.left, root.right];
+
+  while (queue.length) {
+    const leftNode = queue.shift();
+    const rightNode = queue.shift();
+
+    if (!leftNode && !rightNode) continue;
+    if (leftNode?.val !== rightNode?.val) return false;
+
+    queue.push(leftNode.left, rightNode.right);
+    queue.push(leftNode.right, rightNode.left);
+  }
+
+  return true;
+};
